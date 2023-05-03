7f3b34fe25ed0d01a260845eb0e68a69fa676d3e
BarrySong97 <524000659@qq.com>
2023-05-03 11:10:47 +0800

    feat(年度视图): 添加最大连续天数和最新连续天数

--- src/components/YearlyModal/index.tsx
+++ src/components/YearlyModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal, { ModalReactProps } from "@douyinfe/semi-ui/lib/es/modal";
 import CalendarHeatmap from "react-calendar-heatmap";
 import { FC } from "react";
+import { getMaxConsecutiveDays } from "../../utils/date";
+import { Descriptions } from "@douyinfe/semi-ui";
 export interface YearlyModalProps extends ModalReactProps {
   dates?: Date[];
 }
@@ -11,8 +13,30 @@ const YearlyModal: FC<YearlyModalProps> = ({ dates, ...props }) => {
       count: 1,
     };
   });
+  const consecutiveDays = getMaxConsecutiveDays(dates ?? []);
+  const data = [
+    { key: "目前连续天数", value: consecutiveDays.countFromLast },
+    { key: "最大连续天数", value: consecutiveDays.maxCount },
+  ];
+  const style = {
+    boxShadow: "var(--semi-shadow-elevated)",
+    backgroundColor: "var(--semi-color-bg-2)",
+    borderRadius: "4px",
+    padding: "10px",
+  };
   return (
     <Modal {...props} width={800}>
+      <div className="mb-6 flex">
+        {data.map((v) => {
+          return (
+            <div style={style} className="flex-1 mr-2" key={v.key}>
+              <div>{v.value}</div>
+              <div>{v.key}</div>
+            </div>
+          );
+        })}
+      </div>
+
       <CalendarHeatmap
         showWeekdayLabels
         classForValue={(value) => {
